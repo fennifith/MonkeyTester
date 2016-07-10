85ba1613f018612190caf4068876fdf42bc9c801
Kartik Arora <chipset95@yahoo.co.in>
2016-07-10 11:35:43 +0530

    Changes in UI and UX - #1

--- app/src/main/java/james/monkeytester/MainActivity.java
+++ app/src/main/java/james/monkeytester/MainActivity.java
@@ -7,20 +7,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean monkey, goat;
-    TextView text;
+    private static final String URL_MONKEY_DOCS = "http://developer.android.com/reference/android/app/ActivityManager.html#isUserAMonkey%28%29";
+    private static final String URL_GOAT_DOCS = "http://developer.android.com/reference/android/os/UserManager.html#isUserAGoat%28%29";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,48 +27,58 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        monkey = ActivityManager.isUserAMonkey();
-        goat = ((UserManager) getSystemService(Context.USER_SERVICE)).isUserAGoat();
-        text = (TextView) findViewById(R.id.data);
+        boolean monkey = ActivityManager.isUserAMonkey();
+        boolean goat = ((UserManager) getSystemService(Context.USER_SERVICE)).isUserAGoat();
+
+        TextView text = (TextView) findViewById(R.id.data);
 
         if (monkey && goat) {
-            text.setText("You are both a monkey and a goat, a new type of monkey-goat hybrid... either a moat or gokey. Names are confusing.");
+            text.setText(R.string.moat_or_gokey);
         } else if (monkey) {
-            text.setText("You are a monkey.");
+            text.setText(R.string.monkey);
         } else if (goat) {
-            text.setText("You are a goat.");
+            text.setText(R.string.goat);
         } else {
-            text.setText("You are neither a monkey or a goat.");
+            text.setText(R.string.neither);
         }
 
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new AlertDialog.Builder(MainActivity.this).setTitle("View Documentation").setPositiveButton("ActivityManager.isUserAMonkey()", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com/reference/android/app/ActivityManager.html#isUserAMonkey%28%29")));
-                    }
-                }).setNeutralButton("UserManager.isUserAGoat()", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com/reference/android/os/UserManager.html#isUserAGoat%28%29")));
-                    }
-                }).create().show();
+                new AlertDialog.Builder(MainActivity.this).setTitle("View Documentation")
+                        .setPositiveButton("ActivityManager.isUserAMonkey()",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                                Uri.parse(URL_MONKEY_DOCS)));
+                                    }
+                                })
+                        .setNeutralButton("UserManager.isUserAGoat()",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                                Uri.parse(URL_GOAT_DOCS)));
+                                    }
+                                })
+                        .create().show();
             }
         });
 
         findViewById(R.id.me).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://theandroidmaster.github.io/")));
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://theandroidmaster.github.io/")));
             }
         });
 
         findViewById(R.id.alex).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/+adueppen")));
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://plus.google.com/+adueppen")));
             }
         });
     }
@@ -78,14 +86,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_view) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/TheAndroidMaster/MonkeyTester")));
-            return true;
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/TheAndroidMaster/MonkeyTester")));
         }
         return super.onOptionsItemSelected(item);
     }
